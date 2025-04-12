74b2f816becd8206a78c47c9a1e46066385e1aaa
DEVENDRA-KUMAR-MAHOUR <devendrk79@gmail.com>
2025-04-13 00:14:59 +0530

    some file chng

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,37 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// Replace with your receiving email address
+$receiving_email_address = 'devendrk79@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'devendrk79@gmail.com';
+// Load the PHP Email Form library
+$php_email_form_path = '../assets/vendor/php-email-form/php-email-form.php';
+if (file_exists($php_email_form_path)) {
+    include($php_email_form_path);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'] ?? 'Anonymous';
+$contact->from_email = $_POST['email'] ?? 'no-reply@example.com';
+$contact->subject = $_POST['subject'] ?? 'New Contact Message';
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// Optional: Use SMTP (recommended for live servers)
+$contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'devendrk79@gmail.com',
+    'password' => 'idpcyqokawrgfxbs', // Consider using an App Password (see below)
+    'port' => '587',
+    'encryption' => 'tls' // recommended
+);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Add form fields
+$contact->add_message($_POST['name'], 'Name');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+// Send the email
+echo $contact->send();
 ?>
